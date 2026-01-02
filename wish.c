44ae6855e3be58eb80bf5e26ab16820bb05fcb16
Jason Wolfe <jason06051998@gmail.com>
2026-01-01 22:57:09 -0600

    Made it so now the line get's broken up into an args[] array and the args get passed to exec()

--- wish.c
+++ wish.c
@@ -7,32 +7,46 @@
 #include <string.h>
 #include <sys/wait.h>
 
+#define MAX_ARGS 64
+
 int main(int argc, char **argv){
 
     char *prompt = "wish> ";
+    char *line = NULL;
     char *command = NULL;
     size_t size = 0;
     ssize_t nread;
 
     while(1){
+        char *args[MAX_ARGS] = {NULL};
         printf("%s", prompt);
-        nread = getline(&command, &size, stdin);
+        nread = getline(&line, &size, stdin);
         if(nread == -1){
             printf("\n");
             exit(0);
         }
 
-        if(strcmp(command, "exit\n") == 0){
+        //Break up line
+        char *token, *line_copy;
+        line_copy = line;
+        const char *delim = "\t\n\v\f\r ";    //Whitespace characters
+        for(int j = 0; token = strsep(&line_copy, delim); j++){
+            if(*token != '\0')
+                args[j] = token;
+        }
+
+
+        if(strcmp(args[0], "exit") == 0){
             printf("Goodbye!\n");
             exit(0);
         }
 
-        if(strcmp(command, "ls\n") == 0){
+        //TO-DO: Don't check for just ls. Break line into tokens and check first token for line and the rest for args.
+        if(strcmp(args[0], "ls") == 0){
             //fork and exec in child process
             pid_t pid = fork();
             if(pid == 0){
                 //Child process
-                char *args[] = {"ls", NULL};
                 execvp("ls", args);
 
                 printf("error in child\n");
@@ -45,8 +59,12 @@ int main(int argc, char **argv){
             else
                 wait(NULL);
         }
-        else
-            printf("%s", command);
+        else{
+            for(int i = 0; args[i]; i++){
+                printf("%s ", args[i]);
+            }
+            printf("\n");
+        }
     }
 
     return 0;
